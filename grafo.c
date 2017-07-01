7d5e1d7c3ddba42acb15908d936c5995798dc679
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 16:55:23 -0400

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -1,1 +1,26 @@
 
+
+#include "grafo.h"
+#include "list.h"
+#include "lista.c"
+
+void destroyGraph (tGrafo *G)
+{
+	clearL(G->lista);
+	G->vertice = 0;
+	G->arcos = 0;
+}
+
+
+tNodo first (tGrafo *G, tNodo v)
+{
+	return v->sig;
+}
+
+int getMark (tGrafo *G, tNodo v)
+{
+	return v->info->marca;
+}
+
+
+
