1b3f40ece64af31a98422e7b0b76cd3dc35eadba
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 17:17:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -10,21 +10,42 @@ int main(){
 	scanf("%d",&num_caminos);
 	
 	int i,aux1,aux2;
+	
+	tGrafo *G;
 	for (i=0; i < num_caminos;i++)
 	{
 		
 		scanf("%d %d",&aux1,&aux2);
 		//cambiar esto..
-		setEdge(G,G.lista[aux1],G.lista[aux2]);
+		setEdge(G,aux1,aux2);
 
 	}
 
-	int consultas;
+	int consultas, j;
 	scanf("%d",&consultas);
-
-	int ciudad_cumple;	
-	bool cumple = 0; //false
-
+	printf(consultas);
+	for (j=0,j<consultas;j++)
+	{
+		//FALTA SCANEAR NUM AMIGOS ... Y LOS QUE SIGUEN
+		int ciudad_cumple;	
+		bool cumple = 0; //false
+		int num_vertices = nVertex(G);
+		int maxmarcas =0;
+		for(i =0;i<num_vertices;i++){
+			if (getMark(G[i])>=maxmarcas)
+			{
+				maxmarcas =getMark(G[i]); 
+				ciudad_cumple=i;
+			}
+		}
+		
+		if (maxmarcas==num_amigos)
+			printf("%d\n",i);
+		else	printf("%d\n",num_ciuds);
+	}
+	
+	
+	
 	//realizar operaciones que digan si cumple o no y la ciudad que cumpla si hay
 
 	
