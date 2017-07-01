e9ddc3eedf7d7728f983c441f4536b4dbefe4e3f
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 18:48:23 -0400

    Update grafo.c

--- grafo.c
+++ grafo.c
@@ -1,26 +1,60 @@
-
-
 #include "grafo.h"
-#include "list.h"
-#include "lista.c"
+#include "lista.h"
+
+void initGraph(tGrafo *G, int n){
+	G->lista = (tLista *)malloc(sizeof(tLista)*n);
+	G->vertices = 0;
+	G->arcos = 0;
+}
 
 void destroyGraph (tGrafo *G)
-{
-	clearL(G->lista);
+{	
+	int i;
+	for(i = 0; i < G->vertice; i++) clearL(G->lista[i]);		
+	free((void *)G->lista);
 	G->vertice = 0;
 	G->arcos = 0;
 }
 
+long int nVertex (tGrafo *G){
+	int n;
+	n = G -> vertices;
+	return n;
+}
 
-tNodo first (tGrafo *G, tNodo v)
+long int nEdges(tGrafo *G){
+	return G->arcos;
+}
+
+tNodo *nextg (tNodo *v)
 {
 	return v->sig;
 }
 
-int getMark (tGrafo *G, tNodo v)
+tNodo *first (tGrafo *G, long int i){
+	tNodo *np;
+	np = G -> lista[i] -> head -> sig;
+	return np;
+}
+
+void setEdge(tGrafo *G,long int v1,long int v2){
+	elemento e1,e2;
+	// modifico ambos elementos
+	e1 -> pos = v1;
+	e2 -> pos = v2;
+	G -> arcos++; //aumento en 1 el numero de arcos
+	//agrego ambos elementos
+	append(G -> lista[v1], e2);
+	append(G -> lista[v2], e1);
+}
+
+long int getMark (tNodo *v)
 {
 	return v->info->marca;
 }
 
+void setMark (tGrafo *G, long int i, long int marca){
+	G -> lista[i] -> head -> info -> mark = marca;
+}
 
 
