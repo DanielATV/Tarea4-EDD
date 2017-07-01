0267612dd7f17dd73e4fff4134b07df411cff454
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 17:22:54 -0400

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -15,16 +15,16 @@ long int nVertex (tGrafo *G);
 // retorna el numero de arcos en el grafo--S
 long int nEdges (tGrafo *G); 
 // Devuelve el primer vecino de un vertice v dado (asume que los
-// vecinos de un vértice están ordenados por número de vértice)--D
+// vecinos de un vertice estan ordenados por número de vertice)--D
 tNodo next(tVertice v); 
 // devuelve el vecino siguiente (i+1) a uno dado (i).
-// Retorna el numero total de vértices si es que no existe--D
+// Retorna el numero total de vertices si es que no existe--D
 tNodo first (tGrafo *G, long int i); 
 // agrega un nuevo arco al grafo entre vertices v1 y v2 (no se
-// pueden agregar nuevos vértices)--S
+// pueden agregar nuevos vertices)--S
 void setEdge (tGrafo *G, long int v1, long int v2); 
 // obtiene la marca asignada a un vertice dado (ciertos algoritmos
-// necesitan marcar los vértices)---F
+// necesitan marcar los vertices)---F
 long int getMark (tGrafo *G, tNodo v); 
 // marca un vertice con un valor dado---D
 void setMark (tGrafo *G, tVertice v, int marca); D
