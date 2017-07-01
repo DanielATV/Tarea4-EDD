23611a762b557ab98bcd2f1cad51eb7fc56e2648
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 18:48:06 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "heap.h"
-//#include "heap.c"
 
+#include "lista.h"
+#include "grafo.h"
 int main(){
+	/*
 	int num_ciuds;
 	int num_caminos;
 	scanf("%d",&num_ciuds);
 	scanf("%d",&num_caminos);
 	
 	int i,aux1,aux2;
+	
+	tGrafo *G;
 	for (i=0; i < num_caminos;i++)
 	{
 		
@@ -19,19 +22,42 @@ int main(){
 
 	}
 
-	int consultas;
+	int consultas, j;
 	scanf("%d",&consultas);
-
-	int ciudad_cumple;	
-	bool cumple = 0; //false
-
+	printf(consultas);
+	for (j=0,j<consultas;j++)
+	{
+		//FALTA SCANEAR NUM AMIGOS ... Y LOS QUE SIGUEN
+		char *consulta;
+		scanf("%s",consulta);
+		//------------------
+		
+		long int ciudad_cumple;	
+		bool cumple = 0; //false
+		long int num_vertices = nVertex(G);
+		long int maxmarcas =0;
+		for(i =0;i<num_vertices;i++){
+			if (getMark(G[i])>=maxmarcas)
+			{
+				maxmarcas =getMark(G[i]); 
+				ciudad_cumple=i;
+			}
+		}
+		
+		if (maxmarcas==num_amigos)
+			printf("%d\n",i);
+		else	printf("%d\n",num_ciuds);
+	}
+	
+	
+	
 	//realizar operaciones que digan si cumple o no y la ciudad que cumpla si hay
 
 	
 	printf("%d\n",consultas);
 	if (cumple) printf("%d\n",ciudad_cumple\n);
 	else	printf("%d\n",num_ciuds);
-	
+	*/
 	return 0;
 
 
