47572eaaaa20737eb79562936b9d2b7ba64ede23
fabianji <fabian.jimenez@sansano.usm.cl>
2017-07-01 17:20:04 -0400

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -1,4 +1,4 @@
-TDA Grafo
+//TDA Grafo
 
 typedef struct grafo{
     tLista *lista; //puntero a 
@@ -6,24 +6,25 @@ typedef struct grafo{
     long int arcos; //numero de arcos
 } tGrafo;
 
-// inicializa un grafo para n vértices
-void initGraph (tGrafo *G, int n); S
-// libera la memoria utilizada por un grafo
-void destroyGraph (tGrafo *G); F
-// retorna el número de vértices en el grafo
-long int nVertex (tGrafo *G); D
-// retorna el número de arcos en el grafo
-long int nEdges (tGrafo *G); S
+// inicializa un grafo para n vértices--S
+void initGraph (tGrafo *G, long int n); 
+// libera la memoria utilizada por un grafo--F
+void destroyGraph (tGrafo *G); 
+// retorna el número de vértices en el grafo--D
+long int nVertex (tGrafo *G); 
+// retorna el número de arcos en el grafo--S
+long int nEdges (tGrafo *G); 
+// Devuelve el primer vecino de un vértice v dado (asume que los
+// vecinos de un vértice están ordenados por número de vértice)--D
+tNodo next(tVertice v); 
 // devuelve el vecino siguiente (i+1) a uno dado (i).
-// Retorna el número total de vértices si es que no existe
-tVertice next(tVertice v); F
-// Devuelve el primer vecino de un vértice i dado
-tVertice first (tGrafo *G, long int i); D
+// Retorna el número total de vértices si es que no existe--D
+tNodo first (tGrafo *G, long int i); 
 // agrega un nuevo arco al grafo entre vértices v1 y v2 (no se
-// pueden agregar nuevos vértices)
-void setEdge (tGrafo *G, tVertice v1, tVertice v2, int peso); S
+// pueden agregar nuevos vértices)--S
+void setEdge (tGrafo *G, long int v1, long int v2); 
 // obtiene la marca asignada a un vértice dado (ciertos algoritmos
-// necesitan marcar los vértices)
-long int getMark (tGrafo *G, tVertice v); F
-// marca un vértice con un valor dado
+// necesitan marcar los vértices)---F
+long int getMark (tGrafo *G, tNodo v); 
+// marca un vértice con un valor dado---D
 void setMark (tGrafo *G, tVertice v, int marca); D
