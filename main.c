5625b3a6002233c0262788b051e3cfed5ae97e01
godinezseba <sebastian.godinez@hotmail.com>
2017-07-09 17:45:47 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #include "grafo.h"
-#include "lista.h"
 
 /********************************************************
 *El algoritmo desarrolado hace lo siguiente:            *
@@ -18,12 +17,9 @@
 *grafo.                                                 *
 ********************************************************/
 int main(){
-    
-    /* Declaracion de variables */
     long int num_ciuds;
     long int num_caminos;
     long int consultas;
-
     long int num_amigos, k,i;
     long int ciudad;
     long int ciudad_cumple;
@@ -31,50 +27,50 @@ int main(){
     tGrafo *G;
     tLista *inter;
     //pide el numero de ciudades(nodos) y caminos para trabajar.
+    /* Creacion del grafo */
     scanf("%li\n",&num_ciuds);
     G =initGraph(num_ciuds);
-    
+    //ahora crea los caminos
     /* Adicion de los arcos */
     scanf("%li\n",&num_caminos);
     for (i=0; i < num_caminos;i++){
         scanf("%ld %ld\n",&aux1,&aux2);
         setEdge(G,aux1,aux2);
     }
-    
-    /* Procesamiento de las consultas */
+    //Aca empiezan las consultas!!
+     /* Procesamiento de las consultas */
     scanf("%li\n",&consultas);
     printf("%ld\n",consultas );
-
     for (i = 0 ; i < consultas ; i++){
-
         /* Output en el caso de ser vacia la interseccion */
-        ciudad_cumple = num_ciuds;
-        
+        ciudad_cumple = num_ciuds; //lo definimos de esta manera para si nadie
+        //cumple no haga cambios
         scanf("%li",&num_amigos);
+        //para el primero lo pido aparte para crear la lista inicial de personas
         scanf("%ld",&ciudad);
-
+        /* Obtencion de la los vecinos del primer amigo */
         inter = cpyL(Vecinos(G, ciudad)); //de esta manera tengo con quien hacer interseccion
         // ahora intersecto esta lista con los amigos
+        /* Se realiza la interseccion de los vecinos de todos los amigos */
         for(k = 0; k < num_amigos -1 ; k++){
-            
             scanf(" %ld",&ciudad);
             inter = Intersection(inter,Vecinos(G,ciudad));
-
         }
-        
+        //si algun valor sobrevive despues de la interseccion se busca el menor
+        //de ellos para ser la ciudad que cumpla
         /* Se busca el menor de la interseccion */
         if(length(inter) != 0)
           for (moveToStart(inter); currPos(inter) < length(inter); next(inter))
             if (getValue(inter).ciudad < ciudad_cumple)
               ciudad_cumple = getValue(inter).ciudad;
-
+        /* Output del problema */
         //aca imprime en pantalla la ciudad que cumple
         printf("%li\n",ciudad_cumple); //no necesita condiciones, porque si nadie
         //cumple ciudad_cumple predeterminadamente sera la cantidad de ciudades
+        /* Liberacion de memoria de la lista */
         clearL(inter);
     }
-
-
+    /* Liberacion de memoria del grafo */
     destroyGraph(G);
     return 0;
 }
