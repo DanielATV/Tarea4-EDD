f903d1f3fd5042d4506d0a6fca72e0e654bd5033
DanielATV <daniel.torov@sansano.usm.cl>
2017-07-09 20:16:35 -0400

    Update lista.h

--- lista.h
+++ lista.h
@@ -20,35 +20,34 @@ typedef struct Lista{
     long int mark; //marca
 } tLista;
 
-// crea la lista LISTO
+// crea la lista
 tLista *crearLista();
-// borra todos los elementos de la lista, reinicializándola vacía D
+// borra todos los elementos de la lista, reinicializándola vacía 
 void clear (tLista*);
-// inserta un elemento en la posición actual de la lista S LISTO
+// inserta un elemento en la posición actual de la lista 
 int insert (tLista*,elemento);
-// agrega un elemento al final de la lista LISTO
+// agrega un elemento al final de la lista
 int append (tLista*,elemento);
-// borra el elemento actual y retorna su valor  F LISTO
-elemento lremove (tLista*);
-// mueve la posición actual al comienzo de la lista D LISTO
+// borra el elemento actual y retorna su valor
+// mueve la posición actual al comienzo de la lista 
 void moveToStart (tLista*);
-// mueve la posición actual al final de la lista S LISTO
+// mueve la posición actual al final de la lista
 void moveToEnd (tLista*);
 // mueve la posición actual al siguiente elemento de la lista.
-// No produce cambios si está en la cabeza de la lista F LISTO
+// No produce cambios si está en la cabeza de la lista 
 int next (tLista*);
 // mueve la posición actual al elemento anterior de la lista.
-// No produce cambios si está en la cola de la lista D LISTO
+// No produce cambios si está en la cola de la lista
 void prev (tLista*);
-// retorna el número de elementos en la lista s LISTO
+// retorna el número de elementos en la lista
 long int length (tLista*);
-// retorna la posición del elemento actual F LISTO
+// retorna la posición del elemento actual
 long int currPos (tLista*);
-// mueve la posición actual a una especificada D(puede no necesario)
+// mueve la posición actual a una especificada
 void moveToPos (tLista*,int);
-// obtiene el valor del elemento actual de la lista S LISTO
+// obtiene el valor del elemento actual de la lista
 elemento getValue (tLista*);
-// imprime la lista F
+// imprime la lista
 void clearL(tLista*);
 //realiza la Intersection entre dos listas
 tLista *Intersection(tLista *,tLista *);
